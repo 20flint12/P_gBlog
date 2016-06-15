10f544906574d4cd2b84208e4c1cc64bc586e6a6
20flint12 <20flint12@gmail.com>
2016-06-15 10:07:04 +0300

    middleware

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'middleware.filter_ip_middleware.FilterIPMiddleware'
 ]
 
 # ROOT_URLCONF = 'mysite.urls'
