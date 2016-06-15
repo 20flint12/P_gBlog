a50ce3775a9cfbbc472efbd9e21eb915a5117498
20flint12 <20flint12@gmail.com>
2016-06-15 10:26:39 +0300

    middleware++

--- middleware/filter_ip_middleware.py
+++ middleware/filter_ip_middleware.py
@@ -1,8 +1,10 @@
+
 class FilterIPMiddleware(object):
+
     # Check if client IP is allowed
     def process_request(self, request):
-        allowed_ips = ['192.168.1.1', '127.0.0.1'] # Authorized ip's
-        ip = request.META.get('REMOTE_ADDR') # Get client IP
+        allowed_ips = ['192.168.1.1', '127.0.0.1']  # Authorized ip's
+        ip = request.META.get('REMOTE_ADDR')  # Get client IP
         print "REMOTE_ADDR=", ip
         if ip not in allowed_ips:
             # raise Http403 # If user is not allowed raise Error
