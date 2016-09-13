fe85cbb80ccc3d4a0ef14778307b25058362563f
20flint12 <20flint12@gmail.com>
2016-09-13 19:50:28 +0300

    advert ++

--- advert/views.py
+++ advert/views.py
@@ -1,7 +1,6 @@
 
 from django.shortcuts import render
 
-# Create your views here.
 
 from django.shortcuts import render
 from django.utils import timezone
@@ -14,8 +13,6 @@ from django.http import HttpResponse
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
-
-
 
 
 
